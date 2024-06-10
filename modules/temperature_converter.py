4b53f432239e844a4ef0a33d39318e5ba21301c1
TheManufacturer <antonio.mario.demarco@gmail.com>
2024-06-10 12:44:58 +0200

    Create a Basic Temperature converter _OK

---
+++ modules/temperature_converter.py
@@ -0,0 +1,24 @@
+def celsius_to_fahrenheit(celsius):
+    return (9/5) * celsius + 32
+
+def fahrenheit_to_celsius(farhrenheit):
+    return (5/9) * (farhrenheit - 32)
+
+def main_temp():
+    print("Select a Converter :")
+    print("1. Convert Celsius to Fahrenheit")
+    print("2. Convert Fahrenheit to Celsius")
+
+    selection = input("Type choose")
+
+    if selection == '1':
+        celsius = float(input("Type Celsius grade "))
+        print("The converted value is : ", celsius_to_fahrenheit(celsius), "°")
+    elif selection == '2':
+        fahrenheit = float(input("Type Fahrenheit grade "))
+        print("The converted value is : ", fahrenheit_to_celsius(fahrenheit), "°")
+    else:
+        print("Sure are u write correct test ?")
+
+if __name__ == "__main__":
+    main_temp()
