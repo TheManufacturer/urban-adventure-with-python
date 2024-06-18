dd461b1ca19fafd9e5c8aef2a9ae5bafe492a335
TheManufacturer <antonio.mario.demarco@gmail.com>
2024-06-18 15:57:40 +0200

    ToDo exercise_dictionary_simply

---
+++ modules/simply_data_structures.py
@@ -0,0 +1,74 @@
+# Define a tuple with subject names
+subjects = ("English", "Physics", "Geography", "Mathematics")
+
+
+def exercise_tuple_simply():
+    print("In this method we have le max length of element in tupla")
+    print(f"the subjects : {subjects}")
+
+    # Initialize a variable to find the longest subject name
+    max_length_subject = subjects[0]
+
+    # Loop to find the longest subject name
+    for subject in subjects:
+        # Check if the current subject name is longer than the current max_length_subject
+        if len(subject) > len(max_length_subject):
+            max_length_subject = subject
+
+    # Print the longest subject name found
+    print(f"Subject with the longest name is: {max_length_subject}")
+
+
+def exercise_tuple_index():
+    print("Here we can have (index and value)of tupla   --> using enumerate function")
+
+    # Use enumerate to get both index and value
+    # [return for ex: ("English", 0) so, we can have index and value]
+    for index, value in enumerate(subjects):
+        print(f"Element : {value}, position n° {index} ")
+
+
+def exercise_dictionary_simply():
+    pass
+
+
+def simply_data_exercise():
+    exercise_tuple_index()
+    print("\n")
+    exercise_tuple_simply()
+
+
+if __name__ == "__main__":
+    simply_data_exercise()
+
+'''
+--- Esercizio sulle Tuple: (Accesso ed Informazioni)
+
+- Descrizione: 
+    Definisci una tupla contenente nomi di materie scolastiche. 
+    Per ciascuna materia, stampa il nome e la lunghezza della parola più lunga.
+    
+    --> "English", "Physics", "Geography", "Mathematics"
+
+- Obiettivo: 
+    Esercitare l'accesso agli elementi di una tupla e l'utilizzo 
+    del metodo len() per ottenere la lunghezza di una stringa.
+
+-- Dettagli aggiuntivi: 
+    
+    Includi anche un esempio su come accedere agli elementi di una tupla 
+    utilizzando indici numerici.
+
+---Esercizio sui Dizionari: (Aggiornamento e Stampa)
+
+- Descrizione: 
+    Crea un dizionario iniziale con alcuni studenti e i loro voti in alcune materie. 
+    Consentire all'utente di aggiornare i voti di uno studente e stampare il dizionario aggiornato.
+
+- Obiettivo: 
+    Esercitare l'aggiornamento di valori in un dizionario e la stampa del dizionario.
+
+- Dettagli aggiuntivi: 
+    Includi un esempio su come aggiungere uno studente al dizionario e 
+    come stampare il dizionario completo dopo l'aggiornamento.
+'''
